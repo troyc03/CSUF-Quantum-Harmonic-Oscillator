a7673a83935596dce52747d96c43a5d93042fd45
troyc03 <tchin@django.com>
2024-12-31 16:56:33 -0800

    Added starter code to visualization.py

--- OscillatorLab/visualization.py
+++ OscillatorLab/visualization.py
@@ -7,12 +7,44 @@ Purpose: Handles all visualization modules to graphically solve the Schrodinger
 Equation.
 """
 
+import matplotlib.pyplot as plt
+
 class QuantumVisualizer:
 
     @staticmethod
     def plot_wavefunctions(x, wavefunctions, num_wavefunctions=5):
-        pass
+        """
+        Plot the wavefunctions.
+
+        Parameters:
+            x (np.ndarray): Grid points.
+            wavefunctions (np.ndarray): Normalized wavefunctions.
+            num_wavefunctions (int): Number of wavefunctions to plot.
+        """
+        plt.figure(figsize=(8,6))
+        for i in range(num_wavefunctions):
+            plt.plot(x, wavefunctions[:,i], label="psi_{i}(x)")
+        plt.title("Wavefunctions")
+        plt.xlabel("x")
+        plt.ylabel("Psi")
+        plt.legend()
+        plt.grid()
+
 
     @staticmethod
     def plot_energy_levels(eigenvalues, num_levels=5):
-        pass
+        """
+        Plot the energy levels.
+
+        Parameters:
+            eigenvalues (np.ndarray): Array of eigenvalues (energy levels).
+            num_levels (int): Number of energy levels to plot.
+        """
+        plt.figure(figsize=(10,6))
+        plt.hlines(eigenvalues[:num_levels], xmin=0, xmax=1, color='red', linestyles='dashed')
+        plt.title("Energy Levels")
+        plt.xlabel("")
+        plt.ylabel("Energy levels (E)")
+        plt.xticks([])
+        plt.legend()
+        plt.show()
